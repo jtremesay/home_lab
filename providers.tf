1ffee68731b58bc5ed9cf9d531ff9bcde29944ae
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-03-27 00:44:32 +0100

    use kubernetes as backend

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "2.36.0"
     }
   }
+
+  backend "kubernetes" {
+    secret_suffix = "state"
+    config_path    = "~/.kube/config"
+    config_context = "home"
+  }
 }
 
 provider "kubernetes" {
